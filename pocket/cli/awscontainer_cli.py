a017b87f986b4f9130af1d974d4cf0081adfd11d
Masaaki Yasui <masaakiyasui@gmail.com>
2024-01-30 17:27:55 +0800

    Update awscontainer_cli

--- pocket/cli/awscontainer_cli.py
+++ pocket/cli/awscontainer_cli.py
@@ -1,3 +1,4 @@
+import webbrowser
 from pprint import pprint
 
 import click
@@ -26,3 +27,67 @@ def get_awscontainer_resource(stage):
 def yaml(action, stage):
     ac = get_awscontainer_resource(stage)
     pprint(ac.stack.yaml)
+
+
+@awscontainer.command()
+@click.option("--stage", prompt=True)
+def yaml_diff(stage):
+    ac = get_awscontainer_resource(stage)
+    pprint(ac.stack.yaml_diff)
+
+
+@awscontainer.command()
+@click.option("--stage", prompt=True)
+def create(stage):
+    ac = get_awscontainer_resource(stage)
+    if not ac.status == "NOEXIST":
+        echo.warning("AWS lambda container is already created.")
+    else:
+        ac.create()
+        echo.success("Created: lambda")
+
+
+@awscontainer.command()
+@click.option("--stage", prompt=True)
+def update(stage):
+    ac = get_awscontainer_resource(stage)
+    if ac.status == "NOEXIST":
+        echo.warning("AWS lambda has not created yet.")
+        return
+    if ac.status == "FAILED":
+        echo.danger("AWS lambda has failed. Please check console.")
+        return
+    if ac.status == "PROGRESS":
+        echo.warning("AWS lambda is updating. Please wait.")
+        return
+    ac.update()
+
+
+@awscontainer.command()
+@click.option("--stage", prompt=True)
+def status(stage):
+    ac = get_awscontainer_resource(stage)
+    if ac.status == "COMPLETED":
+        echo.success("Container is working!!!")
+    elif ac.status == "NOEXIST":
+        echo.warning("Container has not created yet.")
+    elif ac.status == "FAILED":
+        echo.danger("Container has failed. Please check console.")
+    else:
+        echo.warning("Container stack status: %s" % ac.stack.status)
+
+
+@awscontainer.command()
+@click.option("--stage", prompt=True)
+@click.option("--open", is_flag=True, default=False)
+def url(stage, open):
+    ac = get_awscontainer_resource(stage)
+    if ac.status == "COMPLETED":
+        if "wsgi" in ac.endpoints:
+            echo.success("wsgi url: %s" % ac.endpoints["wsgi"])
+            if open:
+                webbrowser.open(ac.endpoints["wsgi"])
+        else:
+            echo.warning("wsgi endpoint not found.")
+    else:
+        echo.warning("Container is not working.")
